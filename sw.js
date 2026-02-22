49e0702f56ab5771da64167c41c0e8ebfa194d31
Dubinspector <tomaspisa94@gmail.com>
2026-02-22 21:24:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lol-counter-quiz-v31";
+const CACHE = "lol-counter-quiz-v32";
 const ASSETS = [
   "./",
   "./index.html",
@@ -46,6 +46,7 @@ self.addEventListener("fetch", (event) => {
     }))
   );
 });
+
 
 
 
